50fe3bc0a5801b0da9230b1e7ffd815569b2a64d
André Miragaia <andre.miragaia@ua.pt>
2024-02-18 22:54:26 +0000

    up

--- AG/main_app.cpp
+++ AG/main_app.cpp
@@ -8,11 +8,23 @@
 
 
 int main() {
-    //message_handler() do get key; RX (input etsi(open_connect,get_key,close,status), output parametros)
-  
-    //get_key();
-    //INITIALIZE LOADASCII
     
+    //message_handler() do get key; RX (input etsi(open_connect,get_key,close,status), output parametros)
+    auto inputSignalRX = std::make_shared<Message>();
+    auto outputSignalRX = std::make_shared<HandlerMessage>();
+    DestinationTranslationTable destinationTranslationTableRX;
+    destinationTranslationTableRX.add("destinationBlockRX", 0);
+    InputTranslationTable inputDescriptionRX;
+    inputDescriptionRX.add(0, {"destinationBlockRX", "sourceBlock", 5, 0});
+    MessageHandler messageHandlerRX({inputSignalRX.get()}, {outputSignalRX.get()}, 
+                                  destinationTranslationTableRX, FUNCTIONING_AS_RX, inputDescriptionRX);
+    messageHandlerRX.initialize();
+    
+    //handle callback etsi;
+
+    //message handler tx
+    
+    //INITIALIZE LOADASCII
     auto outputSignalLoadAscii = std::make_shared<Binary>();
     LoadAscii loadAscii({}, {outputSignalLoadAscii.get()});
     std::cout << "Iniciando..." << std::endl;
@@ -23,11 +35,14 @@ int main() {
     loadAscii.setSamplingPeriod(1);
     loadAscii.setDataType(signal_value_type::t_binary);
     loadAscii.setDelimiterType(LoadAscii::delimiter_type::ConcatenatedValues);
-
-
     loadAscii.initialize();
+
+
+
+    //message handler rx
+
     //INITIALIZE MESSAGE HANDLER TX
-    auto outputSignalTX = std::make_shared<HandlerMessage>();
+    auto outputSignalTX = std::make_shared<Binary>();
     DestinationTranslationTable destinationTranslationTable;
     destinationTranslationTable.add("destinationBlockTX", 0);
     InputTranslationTable inputDescription;
@@ -37,6 +52,11 @@ int main() {
                                   destinationTranslationTable, FUNCTIONING_AS_TX, inputDescription);
     messageHandlerTX.initialize();
     
+    //IP TUNNEL
+
+    
+    //INITIALIZE MESSAGE HANDLER RX
+    /*
     auto inputSignalRX = std::make_shared<Message>();
     auto outputSignalRX = std::make_shared<Binary>();
     DestinationTranslationTable destinationTranslationTableRX;
@@ -48,26 +68,28 @@ int main() {
     messageHandlerRX.initialize();
     
     
-    SaveAscii saveAscii({outputSignalRX.get()}, {});
+    SaveAscii saveAscii({outputSignalTX.get()}, {});
     saveAscii.setAsciiFileName("rx_raw");
-    saveAscii.setAsciiFileNameExtension(".dat");
+    saveAscii.setAsciiFileNameExtension("dat");
     saveAscii.setAsciiFileNameTailNumber("0");
     saveAscii.setAsciiFolderName("key_files");
     saveAscii.setAsciiFileNameTailNumberModulos(0);
     saveAscii.setDataType(signal_value_type::t_binary);
     saveAscii.setDelimiterType(SaveAscii::delimiter_type::ConcatenatedValues);
     saveAscii.initialize();
-
+    */
     
     int i = 0;
     while (true) {
+        //buffer size 512
         if (outputSignalLoadAscii->space() == 0 || !loadAscii.runBlock()) {
-            
+        
+            //ip tunnel
             messageHandlerTX.runBlock();
-            messageHandlerRX.runBlock();
+            //-> etsi
             i++;
             printf("i = %d\n", i);
-            saveAscii.runBlock(); // Save processed data to file
+            
             // Se não há espaço no buffer ou não há mais dados para ler, processar os dados
             outputSignalLoadAscii->reset();
             
