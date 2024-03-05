f88b32479140db352ce94986c96e07eb13ce5421
jphcr <jphcr@ua.pt>
2024-03-05 12:39:41 +0000

    load_request atualizado

--- client/load_request.cpp
+++ client/load_request.cpp
@@ -6,22 +6,33 @@ void LoadRequest::initialize(void){
 }
 
 bool LoadRequest::runBlock(void){
-    int space = outputSignals[0]->space();
-    signal_value_type sTypeOut = outputSignals[0]->getValueType();
+    // int space = outputSignals[0]->space();
+    // signal_value_type sTypeOut = outputSignals[0]->getValueType();
     t_string request = "{\"command\":\"OPEN_CONNECT\",\"data\":{\"source\":\"source\",\"destination\":\"destination\",\"qos\":{\"key_chunk_size\":3,\"max_bps\":5,\"min_bps\":1,\"jitter\":4,\"priority\":5,\"timeout\":0,\"ttl\":10,\"metadata_mimetype\":\"metadata\"},\"key_stream_id\":\"key_stream_id\"}";
-    int requestSize= request.size();
-    
-    if(getFirstTime()){
+
+    // int requestSize= request.size();
+
+    if (getFirstTime()){
         setFirstTime(false);
-        ready = requestSize;
-    }
-    if (ready == 0){
-        return true;
+        t_message msg;
+        msg.setMessageData(request);
+        outputSignals[0]->bufferPut(msg);
     }
 
-    for (t_char c : request){
-        ready--;
-        outputSignals[0]->bufferPut(c);
-    }
     return false;
+    
+    // if(getFirstTime()){
+    //     setFirstTime(false);
+    //     ready = requestSize;
+    // }
+    // if (ready == 0){
+    //     return false;
+    // }
+    
+
+    // for (t_char c : request){
+    //     ready--;
+    //     outputSignals[0]->bufferPut(c);
+    // }
+    
 }
