50aaa126d0d1e12e32c96fde0b2ce9aff2fdd907
jphcr <jphcr@ua.pt>
2024-05-10 22:52:45 +0100

    peer comm add, refresh keysync block

--- include/KeySync_Block.h
+++ include/KeySync_Block.h
@@ -1,7 +1,7 @@
 #define PROGRAM_KEY_SYNC_BLOCK_H_
 
 #include "netxpto_20200819.h"
-#include "etsi_qkd_004.h"
+#include "peer_comm.h"
 #include "json.hpp"
 
 
@@ -14,9 +14,47 @@ public:
         void initialize(void);
         bool runBlock(void);
 
-private:
+        void setSource(key_sync::URI source){
+            this->source = source;
+        }
+        
+        key_sync::URI getSource(){
+            return source;
+        }
 
+        void setDestination(key_sync::URI destination){
+            this->destination = destination;
+        }
+
+        key_sync::URI getDestination(){
+            return destination;
+        }
+
+        void setQoS(unsigned int key_type = 0, unsigned int key_chunk_size = 0){        
+            this->qos.key_type = key_type;
+            this->qos.key_chunk_size = key_chunk_size;
+        }
+
+        key_sync::QoS getQoS() const{
+            return qos;
+        }
+
+private:
+    std::vector<t_message> receivedMessages = {};
+
+    std::vector<t_string> receivedIndexes = {};
+
+    std::vector<t_string> indexes;
+    
     t_message m_index;
     t_integer ready;
+
+    json msgJson;
+    json msgCommand;
+    json msgData;
+
+    key_sync::URI source = "source";
+    key_sync::URI destination = "destination";
+    key_sync::QoS qos;
 
 };
---
+++ lib/peer_comm.cpp
@@ -0,0 +1,35 @@
+#include "peer_comm.h"
+
+json key_sync::KEY_SYNC(const URI &source, const URI &destination, const Status status, IndexBuffer &indexes,const QoS &qos){
+    json key_sync_json ={
+        {"command", "KEY_SYNC"},
+        {"data", {
+            {"source", source},
+            {"destination", destination},
+            {"qos", {
+                {"key_type", qos.key_type},
+                {"key_chunk_size", qos.key_chunk_size},
+            }
+        },
+        },
+        },
+        {"status", status},
+        {"index", indexes},
+
+    };
+    return key_sync_json;
+}
+
+json key_sync::NEW_KEY(const URI &source, const URI &destination, const Status status, const UUID &key_stream_id, IndexBuffer &indexes){
+    json new_key_json={
+        {"command", "NEW_KEY"},
+        {"data", {
+            {"source", source},
+            {"destination", destination},
+        },
+        },
+        {"status", status},
+        {"index", indexes},
+    };
+    return new_key_json;
+}
